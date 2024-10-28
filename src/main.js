c64f79dea0a2fe80683352b50cac0ebe07aaae4b
Luke <luke.gemdall01@gmail.com>
2024-10-28 15:44:47 -0600

    did config

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+//// @ts-check
 import './css/style.css'
+import Phaser from `phaser`
+
+/** @type { Phaser.Core.Config}*/
+const config = {
+width: 600,
+height: 300,
+};
+
+const game = new Phaser.Game(config);
